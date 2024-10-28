17718dc98bdee71a496bd15413f0369433103464
Gs-ygc <95555045+Gs-ygc@users.noreply.github.com>
2024-10-28 14:21:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	fp := gofeed.NewParser()
-	feed, err := fp.ParseURL("https://chegva.com/feed/")
+	feed, err := fp.ParseURL("https://gs2ygc.online/")
 	if err != nil {
 		panic(err)
 	}
